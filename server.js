adf709541bc240cc1ce725b86eb46f1552bb9cbf
Revan Shahbazov <sahbazovrevan03@gmail.com>
2025-04-23 13:31:13 +0400

    ended api

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db/db.json');
-const middlewares = jsonServer.defaults();
+// server.js
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db/db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-server.use(router);
-
-module.exports = server;
+server.use(middlewares)
+server.use('', router)
+server.listen(3333, () => {
+  console.log('JSON Server is running port: 3333')
+})

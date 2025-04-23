e87f10aae31b83c16018ea9d6cd7d5cb9fa170fa
Revan Shahbazov <sahbazovrevan03@gmail.com>
2025-04-23 13:00:19 +0400

    last commit

--- api/menus.js
+++ api/menus.js
@@ -1,10 +1,11 @@
-import fs from 'fs';
-import path from 'path';
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db/db.json');
+const middlewares = jsonServer.defaults();
 
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'db', 'db.json');
-  const fileData = fs.readFileSync(filePath, 'utf8');
-  const json = JSON.parse(fileData);
+server.use(middlewares);
+server.use(router);
 
-  res.status(200).json(json.menus);
-}
+server.listen(process.env.PORT || 3001, () => {
+  console.log('JSON Server is running');
+});
